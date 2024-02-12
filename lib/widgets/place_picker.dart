49dc802119dc8d0a613f1ef37d3b44e337acf0a8
Eslam Hanafy <ehanafy@arabdt.com>
2024-02-12 12:28:53 +0200

    Upgrading the packages

--- lib/widgets/place_picker.dart
+++ lib/widgets/place_picker.dart
@@ -7,7 +7,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:place_picker/entities/entities.dart';
-import 'package:place_picker/entities/localization_item.dart';
 import 'package:place_picker/widgets/widgets.dart';
 
 import '../uuid.dart';
@@ -256,7 +255,7 @@ class PlacePickerState extends State<PlacePicker> {
       ),
     );
 
-    Overlay.of(context)!.insert(this.overlayEntry!);
+    Overlay.of(context).insert(this.overlayEntry!);
 
     autoCompleteSearch(place);
   }
@@ -370,7 +369,7 @@ class PlacePickerState extends State<PlacePicker> {
       ),
     );
 
-    Overlay.of(context)!.insert(this.overlayEntry!);
+    Overlay.of(context).insert(this.overlayEntry!);
   }
 
   /// Utility function to get clean readable name of a location. First checks
